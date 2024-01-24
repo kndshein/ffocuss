4d093133e0af2a396b53b6e079b065c042891c20
kndshein <kndshein@gmail.com>
2024-01-24 00:07:19 -0800

    Set maxRichResults + add comments to user.js

--- user.js
+++ user.js
@@ -1,6 +1,11 @@
-// Enables customization (required)
+// Enables customization
 user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true);
 
+// Enables the use of context properties in FF
 user_pref('svg.context-properties.content.enabled', true);
 
+// Enables `:has()` CSS on FF
 user_pref('layout.css.has-selector.enabled', true);
+
+// Disable drop down when typing in url (optional)
+user_pref('browser.urlbar.maxRichResults', 0);
