a205f4fee7308e85805a7ccdf8909db92f8aa02e
kndshein <kndshein@gmail.com>
2024-01-23 23:59:30 -0800

    Add popup gap for extensions as well

--- user.js
+++ user.js
@@ -1,3 +1,6 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", true);
-user_pref("layout.css.has-selector.enabled", true);
+// Enables customization (required)
+user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true);
+
+user_pref('svg.context-properties.content.enabled', true);
+
+user_pref('layout.css.has-selector.enabled', true);
